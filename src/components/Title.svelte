55009fcf3558633f5fb52a369d05198abd2ca45e
Luiz Fernando Veríssimo <62751844+luizfverissimo@users.noreply.github.com>
2021-10-30 12:38:41 -0300

    feat: initial commit

---
+++ src/components/Title.svelte
@@ -0,0 +1,5 @@
+<script>
+  export let title = 'texto padrão'
+</script>
+
+<h1 class="text-red-500 text-5xl">{title}</h1>
---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<script>
+  import Title from "../components/Title.svelte";
+</script>
+
+<Title title="Olá Mundão, novamente!" />
+<p>Olá SvelteComponent, será que vamos ser bons amigos?</p>
